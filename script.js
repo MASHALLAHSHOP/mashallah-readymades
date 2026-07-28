c0cacc7d1310d1377a827b35b2a237ac356912d7
MASHALLAH Readymade's Textile's <subhani.70806@gmail.com>
2026-07-28 12:47:09 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Mashallah Website Loaded Successfully!");
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        card.addEventListener("mouseenter", () => {
+            card.style.transform = "scale(1.05)";
+        });
+
+        card.addEventListener("mouseleave", () => {
+            card.style.transform = "scale(1)";
+        });
+    });
+});
